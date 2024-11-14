e5d2532ab20cd4964294e2a4f18ea8aee52c61f2
Winicius Alexandre <winiciusalexandre@gmail.com>
2024-11-14 12:39:05 -0300

    initial commit

---
+++ src/main/java/com/project/springmusic/SpringmusicApplication.java
@@ -0,0 +1,13 @@
+package com.project.springmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/springmusic/SpringmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
